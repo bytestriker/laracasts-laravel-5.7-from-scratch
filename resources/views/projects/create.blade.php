a53407a48e3f8238aa8661fe093040c4bfea44a3
robertgarrigos <robert@garrigos.cat>
2019-04-14 20:09:27 +0200

    added validation code

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,15 +5,36 @@
 
     <form action="/projects" method="post">
       {{ csrf_field() }}
-      <div>
-        <input type="text" name="title" placeholder="Project title">
+      <div class="field">
+        <label class="label" for="title">Project title</label>
+        <div class="control">
+          <input type="text" name="title" placeholder="Project title" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" required value="{{ old('title') }}">
+        </div>
       </div>
-      <div>
-        <textarea name="description" placeholder="Project description" ></textarea>
+
+      <div class="field">
+        <label class="label" for="description">Project description</label>
+        <div class="control">
+          <textarea name="description" placeholder="Project description" class="textarea {{ $errors->has('description') ? 'is-danger' : '' }}" required>{{ old('description') }}</textarea>
+        </div>
       </div>
-      <div>
-        <button type="submit">Create project</button>
+      <div class="field">
+        <div class="control">
+          <button type="submit" class="button is-link">Create project</button>
+        </div>
       </div>
+
     </form>
+
+    @if ($errors->any())
+      <div class="notification is-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
 
 @endsection
