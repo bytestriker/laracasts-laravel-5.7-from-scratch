c416e99556705981ddb6a8be2a3c27105d03af41
robertgarrigos <robert@garrigos.cat>
2019-04-15 17:00:57 +0200

    added task completition code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,11 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" />
-
+    <style>
+      .is-complete{
+        text-decoration: line-through;
+      }
+    </style>
   </head>
   <body>
 
