26eef9b1fe93da60f649a74dd692ef323fb54f60
robertgarrigos <robert@garrigos.cat>
2019-04-18 15:48:24 +0200

    added missing owner scope line

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -15,7 +15,8 @@ class ProjectsController extends Controller
     }
     public function index()
     {
-      $projects = Project::all();
+      $projects = Project::where('owner_id', auth()->id())->get();
+      
       return view('projects.index', ['projects' => $projects]);
     }
 
