a637e2bbfe0c8a2214964cc799fd1eee3dc5a9a2
robertgarrigos <robert@garrigos.cat>
2019-04-19 11:40:08 +0200

    added missing files

---
+++ app/Notifications/ProjectCreatedNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ProjectCreatedNotification extends Notification
+{
+    use Queueable;
+
+    public $project;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($project)
+    {
+      $this->project = $project;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        dump($this->project);
+        return (new MailMessage)
+                    ->subject('New project' . $this->project->title .' created')
+                    ->line('A new project has been created.')
+                    ->action('See the new project', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
